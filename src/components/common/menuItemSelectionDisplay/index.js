3dd6b36456e4abfb348738cbcdf833c612b75655
David Chi <davidchi31415@gmail.com>
2023-04-30 11:39:56 -0500

    Made login modal non-escapable.

--- src/components/common/menuItemSelectionDisplay/index.js
+++ src/components/common/menuItemSelectionDisplay/index.js
@@ -1,30 +1,55 @@
 import { Grid, GridItem } from '@chakra-ui/react';
 import MenuItemCard from '../menuItemCard';
 
-const MenuItemSelectionDisplay = ({ menuItems, readOnly, category, order, onUpdate }) => {
+const MenuItemSelectionDisplay = ({ menuItems, readOnly, menuOnly, category, order, onUpdate }) => {
     return (
-        <Grid templateColumns='repeat(3, 1fr)' gap={2}>
+        <>
             {
-                menuItems?.length ? menuItems.map(
-                    menuItem =>
-                    menuItem.type === category ?
-                        <GridItem>
-                            <MenuItemCard 
-                                info={menuItem} 
-                                readOnly={readOnly}
-                                subtract={
-                                    order.some(
-                                        item => item.name === menuItem.name
-                                        && item.quantity > 0
-                                    )
-                                }
-                                onUpdate={onUpdate}
-                            />
-                        </GridItem>
-                        : ""
-                ) : ""
+                menuOnly ?
+                <Grid templateColumns='repeat(6, 1fr)' gap={1}>
+                    {
+                        menuItems?.length ? menuItems.map(
+                            menuItem =>
+                            <GridItem>
+                                <MenuItemCard 
+                                    info={menuItem} 
+                                    readOnly={readOnly}
+                                    subtract={
+                                        order.some(
+                                            item => item.name === menuItem.name
+                                            && item.quantity > 0
+                                        )
+                                    }
+                                    onUpdate={onUpdate}
+                                />
+                            </GridItem>
+                        ) : ""
+                    }
+                </Grid> :
+                <Grid templateColumns='repeat(3, 1fr)' gap={2}>
+                    {
+                        menuItems?.length ? menuItems.map(
+                            menuItem =>
+                            menuItem.type === category ?
+                                <GridItem>
+                                    <MenuItemCard 
+                                        info={menuItem} 
+                                        readOnly={readOnly}
+                                        subtract={
+                                            order.some(
+                                                item => item.name === menuItem.name
+                                                && item.quantity > 0
+                                            )
+                                        }
+                                        onUpdate={onUpdate}
+                                    />
+                                </GridItem>
+                                : ""
+                        ) : ""
+                    }
+                </Grid>
             }
-        </Grid>
+        </>
     );
 }
 
