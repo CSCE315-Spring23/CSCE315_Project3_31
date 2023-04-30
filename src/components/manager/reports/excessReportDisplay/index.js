9281efba695db6b1b0dc365b43e705dceb4bd145
atsubyo <art.tyoung@gmail.com>
2023-04-29 19:06:25 -0500

    Completed Full Frontend of reports. Still need to properly setup backend connection

---
+++ src/components/manager/reports/excessReportDisplay/index.js
@@ -0,0 +1,139 @@
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Text,
+  Flex,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalBody,
+  ModalHeader,
+  ModalContent,
+  ModalCloseButton,
+  ModalFooter,
+  useDisclosure,
+  Grid,
+  GridItem,
+  Input,
+  useToast,
+  IconButton
+} from "@chakra-ui/react";
+import {
+  InfoIcon
+} from "@chakra-ui/icons";
+import Database from "../../../../data";
+
+const RestockReportDisplay = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedDate, setSelectedDate] = useState(5000);
+  const [excessItems, setExcessItems] = useState([]);
+  const toast = useToast();
+  const handleSubmit = async (e) => {
+    if (!selectedDate) {
+      toast({
+        title: "ERROR",
+        description: "No date provided.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      })
+      return;
+    }
+    e.preventDefault();
+    const items = await Database.getExcessReport(selectedDate);
+    console.log(items);
+    setExcessItems(items);
+  };
+
+  return (
+    <Flex flexDirection="column" justify="center" justifyContent="center" alignItems="center" textAlign="center" py="0.5em">
+      <Button
+        onClick={() => onOpen()}
+        size="md" fontSize="md"
+        width="75%"
+        colorScheme="primary" variant="solid"
+        p={3} px="2em"
+      >
+        Excess Report
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={handleSubmit}>
+            <ModalHeader>
+              <Text textStyle="body2Semi">Excess Report</Text>
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Flex justify="center">
+                <GridItem>
+                  <Text textStyle="body3">Start Date:</Text>
+                </GridItem>
+                <GridItem>
+                  <Input
+                    type="date"
+                    placeholder="Select date"
+                    onChange={(e) => {
+                      const date = new Date(e.target.value);
+                      setSelectedDate(date);
+                    }}
+                    style={{
+                      background: "#f3f3f3",
+                      border: "1px solid",
+                      padding: "0.5rem",
+                    }}
+                  />
+                </GridItem>
+              </Flex>
+              {excessItems ? (
+                  <Box my={4}>
+                    <Grid templateColumns="repeat(3, 1fr)" gap={0}>
+                      <GridItem>
+                        <Text textStyle="body3Semi">Item</Text>
+                      </GridItem>
+                      <GridItem></GridItem>
+                      <GridItem>
+                        <Text textStyle="body3Semi">Sold (%)</Text>
+                      </GridItem>
+                    </Grid>
+                    {excessItems.map((item) => (
+                      <Grid
+                        key={item.inventory_id}
+                        templateColumns="repeat(3, 1fr)"
+                        gap={0}
+                      >
+                        <GridItem>
+                          <Text textStyle="body3">{item.name}</Text>
+                        </GridItem>
+                        <GridItem></GridItem>
+                        <GridItem>
+                          <Text textStyle="body3">{item.quantity}</Text>
+                        </GridItem>
+                      </Grid>
+                    ))}
+                  </Box>
+                ) : (
+                  <Text>No Excess Sales</Text>
+                )}
+            </ModalBody>
+            <ModalFooter justifyContent="center" gap={1}>
+              <Button type="submit" colorScheme="primary">
+                Generate Report
+              </Button>
+              <IconButton 
+                aria-label="Info"
+                icon={<InfoIcon color="blue.500"/>}
+                size="lg"
+                variant="ghost"
+                _hover={{ bg: "transparent" }}
+              >
+              </IconButton>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </Flex>
+  );
+};
+
+export default RestockReportDisplay;
---
+++ src/components/manager/reports/zreportDisplay/index.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Text,
+  Flex,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalBody,
+  ModalHeader,
+  ModalContent,
+  ModalCloseButton,
+  ModalFooter,
+  useDisclosure,
+  Grid,
+  GridItem,
+  Input,
+  useToast,
+  IconButton
+} from "@chakra-ui/react";
+import {
+  InfoIcon
+} from "@chakra-ui/icons";
+import Database from "../../../../data";
+
+const RestockReportDisplay = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [minimumQty, setMinimumQty] = useState(5000);
+  const [refillItems, setRefillItems] = useState([]);
+  const toast = useToast();
+  const handleSubmit = async (e) => {
+    if (!minimumQty) {
+      toast({
+        title: "ERROR",
+        description: "No minimum value provided.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      })
+      return;
+    }
+    e.preventDefault();
+    const items = await Database.getRestockReport(minimumQty);
+    console.log(items);
+    setRefillItems(items);
+    onClose();
+  };
+
+  return (
+    <Flex flexDirection="column" justify="center" justifyContent="center" alignItems="center" textAlign="center" py="0.5em">
+      <Button
+        onClick={() => onOpen()}
+        size="md" fontSize="md"
+        width="75%"
+        colorScheme="primary" variant="solid"
+        p={3} px="2em"
+      >
+        Z Report
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose} size="md">
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={handleSubmit}>
+            <ModalHeader>
+              <Text textStyle="body2Semi">Z Report</Text>
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            </ModalBody>
+            <ModalFooter justifyContent="center" gap={1}>
+              <Button type="submit" colorScheme="primary">
+                Generate Report
+              </Button>
+              <IconButton 
+                aria-label="Info"
+                icon={<InfoIcon color="blue.500"/>}
+                size="lg"
+                variant="ghost"
+                _hover={{ bg: "transparent" }}
+              >
+              </IconButton>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+    </Flex>
+  );
+};
+
+export default RestockReportDisplay;
