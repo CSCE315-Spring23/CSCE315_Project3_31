26ed56256bdf25d5db145600758cc605d09d34b0
Andrew Mao <andrewmao@Andrews-MacBook-Pro-8.local>
2023-05-02 17:40:22 -0500

    added excress report visualization

--- src/components/manager/reports/excessReportDisplay/index.js
+++ src/components/manager/reports/excessReportDisplay/index.js
@@ -19,15 +19,16 @@ import {
 } from "@chakra-ui/react";
 import Database from "../../../../data";
 import InfoButton from "../../../common/infoButton";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 
 const ExcessReportDisplay = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const [excessItems, setExcessItems] = useState([]);
+	const [chartData, setChartData] = useState([]);
 	const toast = useToast();
 	const handleSubmit = async (e) => {
 		e.preventDefault();
     const selectedDate = document.getElementById("selectDate-input").value;
-
     const today = new Date();
 
 		if (!selectedDate) {
@@ -53,6 +54,17 @@ const ExcessReportDisplay = () => {
 		console.log("easdf");
 		console.log(items);
 		setExcessItems(items);
+
+		const newChartData = items.map((item) => ({
+			name: item.name,
+			Percentage_Sold: item.percentage_sold.toFixed(4),
+			difference: 10 - item.percentage_sold.toFixed(4),
+		  }));
+		  console.log("newChartData: ")
+		  console.log(newChartData);
+	  
+		  setChartData(newChartData);
+
 		toast({
       title: "Excess Report Generated",
       description: "Showing excess inventory from sales since " + selectedDate,
@@ -148,6 +160,23 @@ const ExcessReportDisplay = () => {
 									<Text>No Excess Inventory</Text>
 								)}
 							</Box>
+							{excessItems.length !== 0 && (
+								<BarChart
+									width={500}
+									height={chartData.length * 25}
+									data={chartData}
+									margin={{ top: 25, bottom: 25}}
+									layout="vertical"
+								>
+									<CartesianGrid strokeDasharray="3 3" />
+									<XAxis type="number" domain={[0, 10]} ticks={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} />
+									<YAxis type="category" dataKey="name" hide={true} />
+									<Tooltip />
+									<Legend />
+									<Bar dataKey="Percentage_Sold" stackId="a" fill="#e51636" barSize={20}/>
+									<Bar dataKey="difference" stackId="a" fill="#ffcccb" barSize={20}/>
+							  </BarChart>
+							)}
 						</ModalBody>
 						<ModalFooter justifyContent="center" gap={1}>
 							<Button type="submit" colorScheme="primary">
